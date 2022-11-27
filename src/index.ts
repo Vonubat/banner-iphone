6fd4bab5697261cc2ead9ceb8d81bb0d46b85e38
Tabunov Egor <Vonubat@gmail.com>
2022-11-27 14:14:01 +0300

    feat: add header layout

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './sass/index.scss';
 
 // for testing
 import { createHTMLElement, createSVGElement } from './utils/htmlElementFactory';
+import { Header } from './layout/header';
 
 const h1 = createHTMLElement('h1', {
   innerHtml: `Unlimited Access
@@ -10,5 +11,10 @@ to All Features`,
 
 const svg = createSVGElement('./assets/shape-1.svg', 'shape-1');
 
+const header = new Header().draw();
+
+document.body.appendChild(header);
 document.body.appendChild(svg);
 document.body.appendChild(h1);
+
+document.body.style.background = 'red';
---
+++ src/layout/header.ts
@@ -0,0 +1,35 @@
+import { LocalizationService } from '../services/localizationService';
+import { createHTMLElement, createSVGElement } from '../utils/htmlElementFactory';
+
+export class Header extends LocalizationService {
+  createRestoreAnchor(): HTMLAnchorElement {
+    return createHTMLElement('a', {
+      attributes: [['href', '#']],
+      cssClassList: ['anchor-restore'],
+      innerHtml: this.en.Restore,
+    });
+  }
+
+  createCloseAnchor(): HTMLAnchorElement {
+    const closeAnchor = createHTMLElement('a', {
+      attributes: [['href', '#']],
+      cssClassList: ['anchor-cancel'],
+    });
+    const cancelIcon = createSVGElement('../assets/close.svg', 'close', {
+      boxWidth: '16px',
+      boxHeight: '16px',
+    });
+    closeAnchor.appendChild(cancelIcon);
+    return closeAnchor;
+  }
+
+  draw(): HTMLElement {
+    const header = createHTMLElement('header', {
+      cssClassList: ['header'],
+    });
+    const closeBtn = this.createCloseAnchor();
+    const restoreAnchor = this.createRestoreAnchor();
+    header.append(closeBtn, restoreAnchor);
+    return header;
+  }
+}
